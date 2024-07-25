994c2950a25b97ef651ec06ea308987cb4830ffc
Webioann <ioannn7777@gmail.com>
2024-07-25 14:45:30 +0300

    created start CRUD logic

---
+++ app/components/Todo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Styles/todo.css'
+
+function Todo() {
+    return (
+        <div className='todo'>
+            
+        </div>
+    )
+}
+
+export default Todo
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+const prisma = require('../prisma/prisma.client')
 
 export default function Home() {
+
+  const todo = async() => {
+    await prisma.todo.create({
+      data: {
+        title: 'hello world',
+        isComplited: false
+      }
+    })
+  }
+
   return (
     <main>
       <h1>HOME PAGE</h1>
+      <button onClick={todo}>CLICK ME</button>
     </main>
   );
 }
