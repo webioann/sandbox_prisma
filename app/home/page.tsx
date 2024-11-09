277cf17cab15738428ced8ff2932cc90469bd7a2
Webioann <ioannn7777@gmail.com>
2024-11-09 11:12:02 +0200

    was created Assigned Tasks block UI

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import TotalResult from '@/components/TotalResult/TotalResult';
+import TotalResult from '@/components/TotalResult/TotalResult'
+import { FaPlus } from "react-icons/fa"
 import styles from './home.module.scss'
 
 function HomePage() {
-
+    let tasks = 14
     return (
         <div className={styles.page}>
             {/* ======================= */}
@@ -15,15 +16,35 @@ function HomePage() {
                 <TotalResult title='Overdue Tasks' total={0}/>
             </div>
 
-            <div className="dashboard-wrapper">
-                <div className="task-viewer">
+            <div className={styles.dashboard_wrapper}>
 
+                <div className={styles.tasks_viewer}>
+                    {/* assigned tasks block */}
+                    <div className={styles.assigned_wrapper}>
+                        {/* ========================= */}
+                        <div className={styles.assigned_header}>
+                            <h4 className={styles.title}>Assigned Tasks ({tasks})</h4>
+                            <div className={styles.plus_button}>
+                                <FaPlus size={12} color='grey'/>
+                            </div>
+                        </div>
+                        {/* =========================== */}
+                        <ul className={styles.project_viewer}>
+                            <h1>Hello</h1>
+                        </ul>
+                        {/* ============================ */}
+                        <button className={styles.show_all_button}>
+                            <p>Show All</p>
+                        </button>
+                    </div>
+                    {/* people block */}
+                    
                 </div>
 
-                <div className="project-viewer">
 
-                </div>
+
             </div>
+
         </div>
     )
 }
