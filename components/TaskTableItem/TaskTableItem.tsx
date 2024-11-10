07c23781f0c63399001b513783754d6865cd48d8
Webioann <ioannn7777@gmail.com>
2024-11-10 16:00:29 +0200

    types for project and task data

--- components/TaskTableItem/TaskTableItem.tsx
+++ components/TaskTableItem/TaskTableItem.tsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import { MdOutlineCheckBox, MdOutlineCheckBoxOutlineBlank } from "react-icons/md"
 import { HiDotsVertical } from "react-icons/hi"
+import type { dueDateStatusColors, createAndHighlightDueDateType } from '../../types/tasks.types'
 import styles from './task-table-item.module.scss'
 import BadgeWithSymbol from '../BadgeWithSymbol/BadgeWithSymbol'
 import { PiArrowsDownUp } from "react-icons/pi"
 
 function TaskTableItem() {
-    
-    const newdate = Date.now()
-    console.log(new Date(newdate).getMonth( ));
-    const dates = 1731218644149
+
+    const dueDateDay = new Date(2024, 10, 18).valueOf()
+
+    const createAndHighlightDueDate: createAndHighlightDueDateType = (dueDate: number, criticalPeriod: number) => {
+        const currentDate = Date.now();
+        const month = ["January","February","March","April","May","June","July",
+            "August","September","October","November","December"];
+        const oneDay = 86400000;
+        // create readable date ---
+        let dueDateInReadbleFormat: string = '';
+        let dueDateYear = new Date(dueDate).getFullYear()
+        let dueDateMonth = new Date(dueDate).getMonth()
+        let dueDateDay = new Date(dueDate).getDate()
+        dueDateInReadbleFormat= `${month[dueDateMonth]} ${dueDateDay}th, ${dueDateYear}`
+        // highlight critical period ---
+        let status: dueDateStatusColors = '#34CB97'
+        let daysByDutDate = Math.ceil(( dueDate - currentDate ) / oneDay) 
+        if(daysByDutDate >= criticalPeriod + 2) status = '#34CB97'
+        if(daysByDutDate < criticalPeriod + 2 && daysByDutDate > 2) status = '#F7CE1B'
+        if(daysByDutDate <= 2) status = '#F57174'
+
+        return { dueDateInReadbleFormat, status }
+    } 
+    let { dueDateInReadbleFormat, status } = createAndHighlightDueDate(dueDateDay, 3)
+    // console.log(dueDateInReadbleFormat);
+    // console.log(status);
 
     return (
         <div className={styles.table_item}>
@@ -30,7 +53,12 @@ function TaskTableItem() {
             </div>
 
             <div className={styles.due_date}>
-                <p className={styles.date}>October 7th, 2024</p>
+                <p 
+                    className={styles.date}
+                    style={{color: status}}
+                    >
+                    {dueDateInReadbleFormat
+                }</p>
             </div>
 
             <div className={styles.task_status}>
---
+++ types/tasks.types.ts
@@ -0,0 +1,48 @@
+// types for My Tasks page ====
+// type statusHighlightColorsTypes = ''
+type statusesType = 
+    |'Backlog'
+    |'Todo'
+    |'In Review'
+    |'In Progress'
+    |'Done'
+type statusesListType = {
+    status: statusesType
+    statusHighlightColor: string
+}
+export const taskStatusesList: Array<statusesListType> = [
+    {status: 'Backlog', statusHighlightColor: '#F672B2'},
+    {status: 'Todo', statusHighlightColor: '#F57174'},
+    {status: 'In Review', statusHighlightColor: '#64A3F6'},
+    {status: 'In Progress', statusHighlightColor: '#F7CE1B'},
+    {status: 'Done', statusHighlightColor: '#34CB97'},
+];
+
+export type dueDateStatusColors = Readonly<'#34CB97' | '#F7CE1B' | '#F57174'>
+// function for transforming Dates in readable format and highlighting a critical period
+export type createAndHighlightDueDateType = 
+    (
+        dueDate: number, 
+        criticalPeriod: number
+    ) => 
+    {
+        dueDateInReadbleFormat: string,
+        status: dueDateStatusColors
+    }
+// one single task object type
+interface ITaskType {
+    task_id: string
+    task_checked: boolean
+    assigned_person: string
+    status: statusesType
+    due_date: number
+    task_created_at: number
+}
+export interface IProjectType {
+    project_id: string
+    project_name: string
+    project_tasks_list: Array<ITaskType>
+    project_due_date: number
+    project_created_at: number
+}
+
