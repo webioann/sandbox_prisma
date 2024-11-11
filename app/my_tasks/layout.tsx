5e31f6c713d210471750f197c29d00b85fc93444
Webioann <ioannn7777@gmail.com>
2024-11-11 08:26:26 +0200

    pagination UI

--- app/my_tasks/layout.tsx
+++ app/my_tasks/layout.tsx
@@ -1,17 +1,9 @@
 import React from 'react'
-import TaskPageNavigation from '@/components/TaskPageNavigation/TaskPageNavigation'
 
 export default function TaskPagesLayout({
     children,
     }: {
         children: React.ReactNode
     }) {
-    return (
-        <section style={{width: '100%', height: '100%', padding: '0 1rem'}}>
-            <div style={{border: '1px solid #f4f4f4', borderRadius: '8px', padding: '0 1rem'}}>
-                <TaskPageNavigation/>
-                {children}
-            </div>
-        </section>
-    )
+    return ( <main> {children} </main> )
 }
--- app/my_tasks/table/page.tsx
+++ app/my_tasks/table/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import TaskTableItem from '@/components/TaskTableItem/TaskTableItem'
+import TaskPageNavigation from '@/components/TaskPageNavigation/TaskPageNavigation'
 import { FiCalendar } from "react-icons/fi"
 import { LuListChecks } from "react-icons/lu"
 import { GoPerson } from "react-icons/go"
@@ -17,83 +18,83 @@ import type { ProjectsDataType } from '../../../types/tasks.types'
 
 
 export default function TablePage() {
-
-    const T = () => {
-        let result = ProjectsData.map((project) => {
-            project.project_tasks_list.map(() => {
-
-            })
-        })
-    }
-
+    let D = true
     return (
-        <>
-            <div className={styles.filters_row}>
-                <div className={styles.filter}>
-                    <LuListChecks size={16} color='#060606'/>
-                    <p className={styles.filter_title}>All statuses</p>
-                    <BsChevronExpand size={16} color='#060606'/>
-                </div>
-                <div className={styles.filter}>
-                    <GoPerson size={16} color='#060606'/>
-                    <p className={styles.filter_title}>All assigned</p>
-                    <BsChevronExpand size={16} color='#060606'/>
-                </div>
-                <div className={styles.filter}>
-                    <FaRegFolder size={16} color='#060606'/>
-                    <p className={styles.filter_title}>All projects</p>
-                    <BsChevronExpand size={16} color='#060606'/>
-                </div>
-                <div className={styles.filter}>
-                    <FiCalendar size={16} color='#060606'/>
-                    <p className={styles.filter_title}>Due date</p>
-                </div>
+        <main className={styles.table_page}>
+            <div className={styles.table_wrapper}>
+                <TaskPageNavigation/>
+                {/* FILTERS */}
+                <section className={styles.filters_row}>
+                    <div className={styles.filter}>
+                        <LuListChecks size={16} color='#060606'/>
+                        <p className={styles.filter_title}>All statuses</p>
+                        <BsChevronExpand size={16} color='#060606'/>
+                    </div>
+                    <div className={styles.filter}>
+                        <GoPerson size={16} color='#060606'/>
+                        <p className={styles.filter_title}>All assigned</p>
+                        <BsChevronExpand size={16} color='#060606'/>
+                    </div>
+                    <div className={styles.filter}>
+                        <FaRegFolder size={16} color='#060606'/>
+                        <p className={styles.filter_title}>All projects</p>
+                        <BsChevronExpand size={16} color='#060606'/>
+                    </div>
+                    <div className={styles.filter}>
+                        <FiCalendar size={16} color='#060606'/>
+                        <p className={styles.filter_title}>Due date</p>
+                    </div>
+                </section>
+                {/* TABLE OF TASKS */}
+                <section className={styles.tasks_table}>
+                    {/* TABLE HEADER */}
+                    <div className={styles.table_header}>
+                        <div className={styles.column_name_first}>
+                            <MdOutlineCheckBoxOutlineBlank size={20}/>
+                            <p className={styles.task_name_cell}>Task Name</p>
+                            <TbArrowsUpDown/>  
+                        </div>
+                        <div className={styles.column_name}>
+                            <p>Progect</p>
+                            <TbArrowsUpDown/>  
+                        </div>
+                        <div className={styles.column_name}>
+                            <p>Assignee</p>
+                            <TbArrowsUpDown/>  
+                        </div>
+                        <div className={styles.column_name}>
+                            <p>Due Date</p>
+                            <TbArrowsUpDown/>  
+                        </div>
+                        <div className={styles.column_name}>
+                            <p>Status</p>
+                            <TbArrowsUpDown/>  
+                        </div>
+                    </div>
+                    {/*TABLE ROWS*/}
+                    <ul className={styles.table_of_projects}>
+                        {ProjectsData.map((project) => 
+                            project.project_tasks_list.map((task) => { return (<TaskTableItem project={project} task={task}/>)})
+                        )}
+                    </ul>
+                    
+                    
+                </section>
+                {/* PAGINATION */}
+                <section className={styles.pagination}>
+                    <p className={styles.pagination_diapason}>
+                        {`${0} of ${14} row(s) selected.`}
+                    </p>
+                    <div className={styles.page_switcher}>
+                        <button className={`${D ? styles.active_btn : styles.pagination_btn}`}>
+                            Previous
+                        </button>
+                        <button className={styles.pagination_btn}>
+                            Next
+                        </button>
+                    </div>
+                </section>
             </div>
-
-
-
-            <section className={styles.tasks_table}>
-                <div className={styles.table_header}>
-                    <div className={styles.column_name_first}>
-                        <MdOutlineCheckBoxOutlineBlank size={20}/>
-                        <p className={styles.task_name_cell}>Task Name</p>
-                        <TbArrowsUpDown/>  
-                    </div>
-
-                    <div className={styles.column_name}>
-                        <p>Progect</p>
-                        <TbArrowsUpDown/>  
-                    </div>
-
-                    <div className={styles.column_name}>
-                        <p>Assignee</p>
-                        <TbArrowsUpDown/>  
-                    </div>
-
-                    <div className={styles.column_name}>
-                        <p>Due Date</p>
-                        <TbArrowsUpDown/>  
-                    </div>
-
-                    <div className={styles.column_name}>
-                        <p>Status</p>
-                        <TbArrowsUpDown/>  
-                    </div>
-
-                </div>
-                {/* =================== */}
-                <ul className={styles.table_of_projects}>
-                    {ProjectsData.map((project) => 
-                        project.project_tasks_list.map((task) => { return (<TaskTableItem project={project} task={task}/>)})
-                    )}
-                </ul>
-                
-                
-            </section>
-
-
-
-            <div className={styles.pagination}></div>
-        </>   
+        </main>
     )
 };
