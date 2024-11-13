e5f75a875e1f15308766f2c38999aaf1cea49c56
Webioann <ioannn7777@gmail.com>
2024-11-13 10:06:33 +0200

    was tested auth with using user context

--- app/(authentication)/sign_in/page.tsx
+++ app/(authentication)/sign_in/page.tsx
@@ -1,11 +1,20 @@
+"use client";
+import React from 'react'
+
 import LoginForm from '../components/LoginForm/LoginForm'
+import { UserContext } from '../../../context/UserContextProvider'
+
 import Link from 'next/link';
 import './login.scss'
 
 function SignInPage() {
+        // USER CONTEXT ================>
+        const user = React.useContext(UserContext)
+        // console.log(user);
+    
     return (
         <main className='login-page'>
-            <Link href={'/home'}>Sign In on App</Link>
+            { user ? <Link href={'/home'}>Go to work on Home page</Link> : <h1>NO REGISTER USER</h1>}
             <LoginForm/>
         </main>
     )
